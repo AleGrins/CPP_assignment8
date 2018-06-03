76a1f8fdaf0773590c762b5d57037abdb76d056b
AleGrins <37497692+AleGrins@users.noreply.github.com>
2018-06-03 13:39:44 +0300

    Add files via upload

---
+++ Board.cpp
@@ -0,0 +1,145 @@
+#include "Board.h"
+
+using namespace std;
+
+//display 2D coordinates
+std::ostream& operator<<(std::ostream& os, const Coordinate& ind)
+{
+    int a = int(ind.i);
+    int b = int(ind.j);
+    return os << a << "," << b;
+}
+
+gameChar::gameChar() { c = '.'; }
+
+gameChar::gameChar(const char c_)
+{
+	if(!validChar(c_)) throw IllegalCharException(c_);
+	c = c_;
+}
+
+gameChar& gameChar::operator= (const gameChar& other) { c=other.c; }
+
+bool gameChar::operator== (const gameChar& other) const { return (c == other.c); }		
+bool gameChar::operator== (char c_) const { return (c == c_); }
+bool gameChar::operator!= (const gameChar& other) const { return (c != other.c); }		
+bool gameChar::operator!= (char c_) const { return (c != c_); }
+		
+bool gameChar::validChar(char c) //check that given char is legal
+{
+	if(c == '.' || c == 'X' || c == 'O') return true;
+	return false;
+}
+
+//constructor
+Board::Board(const int N)   
+{
+    boardSize = N;
+    for(int i=0; i<N; i++){
+    	gameChar c = '.';
+    	vector<gameChar> temp;
+    	for(int j=0; j<N; j++){
+    		temp.push_back(c);
+    	}
+    	temp.push_back(c);
+    	board.push_back(temp);
+    }
+}
+
+Board::Board()   
+{
+    boardSize = 3;
+    for(int i=0; i<3; i++){
+    	gameChar c = '.';
+    	vector<gameChar> temp;
+    	for(int j=0; j<3; j++){
+    		temp.push_back(c);
+    	}
+    	temp.push_back(c);
+    	board.push_back(temp);
+    }
+}
+	
+//assignment from other board
+Board& Board::operator= (const Board& other)
+{
+	board = other.board;
+    boardSize = other.boardSize;
+    return *this;
+}
+	
+//assignment from a char, fills entire board with provided char
+Board& Board::operator= (char c)
+{
+	gameChar tmp(c);
+    for(int i=0; i<boardSize; i++){
+    	for(int j=0; j<boardSize; j++){
+    		board[i][j] = tmp;
+    	}
+    }
+    return *this;
+}
+
+//access board member indicated by 2D coordinate ind
+gameChar& Board::operator[](const Coordinate ind) 
+{ 
+	if(!inBounds(ind)) throw IllegalCoordinateException(ind);
+	return board[ind.i][ind.j]; 
+}
+const gameChar& Board::operator[](const Coordinate ind) const
+{
+    if(!inBounds(ind)) throw IllegalCoordinateException(ind);
+    return board[ind.i][ind.j];
+}
+
+//creates an n*n ppm format image representing the board with squares of color
+string Board::draw(const int n)
+{
+    int imgSegLen = n/boardSize; 
+    ofstream boardFile ("TicTacToe_Board.ppm");
+    boardFile << "P6" << endl; //ppm file type identifier
+    boardFile << n << " " << n << endl; //image width and height
+    boardFile << "1" << endl; //maximum color value
+    
+    if (boardFile.is_open()){
+        //
+        for(int i = 0; i < boardSize; i++){
+            for(int j = 0; j < boardSize; j++){
+                if(board[i][j] == '.'){ //an empty square is black
+                    for(int x = (i * imgSegLen); x < ((i + 1) * imgSegLen); x++){
+                        for(int y = (j * imgSegLen); y < ((j + 1) * imgSegLen); y++){
+                            boardFile << 0 << " " << 0 << " " << 0 << endl; //R,G,B values
+                        }
+                    }
+                }
+                else if(board[i][j] == 'X'){ //a square with an X is red
+                    for(int x = (i * imgSegLen); x < ((i + 1) * imgSegLen); x++){
+                        for(int y = (j * imgSegLen); y < ((j + 1) * imgSegLen); y++){
+                            boardFile << 1 << " " << 0 << " " << 0 << endl; //R,G,B values
+                        }
+                    }    
+                }
+                else{ //a square with an O is blue
+                    for(int x = (i * imgSegLen); x < ((i + 1) * imgSegLen); x++){
+                        for(int y = (j * imgSegLen); y < ((j + 1) * imgSegLen); y++){
+                            boardFile << 0 << " " << 0 << " " << 1 << endl; //R,G,B values
+                        }
+                    }    
+                }
+            }
+        }
+        boardFile.close();
+    }
+    else cout << "Unable to open file";
+}
+
+int Board::size() const
+{
+    return boardSize;
+}
+	
+bool Board::inBounds(Coordinate ind) const //check that coordinates are not outside the board
+{
+	if(int (ind.i) < 0 || int (ind.j) < 0 || int (ind.i) >= boardSize || int (ind.j) >= boardSize) return false;
+	return true;
+}
---
+++ Board.h
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <vector>
+#include <exception>
+#include <fstream>
+#include <string>
+using namespace std;
+
+#pragma once
+
+struct Coordinate //used to receive 2D coordinates for the [] overload
+{
+	uint i, j;
+};
+
+//coordinates out of bounds, returns given coordinates
+class IllegalCoordinateException : public std::exception {
+	Coordinate ind;
+    
+    public:
+    	IllegalCoordinateException(Coordinate ind_) : ind (ind_) {}
+
+        Coordinate theCoordinate() const { return ind; }
+};
+
+//illegal character, returns given char
+class IllegalCharException : public std::exception {
+	char c;
+    
+    public:
+    	IllegalCharException(char c_) : c (c_) {}
+
+        char theChar() const { return c; }
+};
+
+//custom char class to accept only specific chars and throw exception otherwise
+class gameChar {
+	public:
+		gameChar();
+		gameChar(const char c_);
+		
+		gameChar& operator= (const gameChar& other);
+		
+		bool operator== (const gameChar& other) const;		
+		bool operator== (char c_) const;
+		bool operator!= (const gameChar& other) const;		
+		bool operator!= (char c_) const;
+		
+		//Output operator
+		friend ostream& operator<< (ostream& output, const gameChar& ch)
+		{ 
+    		output << ch.c;
+    		return output;            
+    	} 
+    	
+    	//Input operator
+    	friend istream& operator>> (istream& input, gameChar& ch)
+    	{
+    		input >> ch.c;
+    		return input;
+    	}
+	
+	private:
+		char c;
+		bool validChar(char c); //check that given char is legal
+};
+
+class Board {
+public:
+	
+	//constructors
+    Board(const int N);
+    Board();
+	
+	//assignment from other board
+	Board& operator= (const Board& other);
+	
+	//assignment from a char, fills entire board with provided char
+	Board& operator= (char c);
+
+	//access board member indicated by 2D coordinate ind
+	gameChar& operator[](const Coordinate ind);
+	const gameChar& operator[](const Coordinate ind) const;
+	
+	int size() const;
+	
+	//display the board
+	friend ostream& operator<< (ostream& output, const Board& b)
+	{ 
+		for(int i=0; i<b.boardSize; i++){
+    		for(int j=0; j<b.boardSize; j++){
+    			output << b.board[i][j];
+    		}
+    		output << endl;
+    	}
+    	return output;            
+    }
+    
+    //Input a board
+    friend istream& operator>> (istream& input, Board& b)
+    {
+    	input >> b.board;
+    	input >> b.boardSize;
+    	return input;
+    }
+    
+    string draw(const int n);
+	
+private:
+	vector<vector<gameChar> >board; 
+	int boardSize;
+	
+	bool inBounds(Coordinate ind) const; //check that coordinates are not outside the board
+};
